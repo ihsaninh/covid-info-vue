cd7b847f78ba134019e714f419c4fba574c35d19
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-03-28 16:24:00 +0700

    Add data global

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,15 @@
         <b-card bg-variant="kuning">
           <div>
             <h6 class="mb-3">Data Kasus Coronavirus di Indonesia Berdasarkan Provinsi</h6>
-            <b-table :items="items" :fields="fields"></b-table>
+            <b-table :items="itemsId" :fields="fieldsId" bordered sticky-header="700px"></b-table>
+          </div>
+        </b-card>
+      </b-card-group>
+      <b-card-group deck class="mt-5">
+        <b-card bg-variant="kuning">
+          <div>
+            <h6 class="mb-3">Kasus Coronavirus Global (Data by JHU)</h6>
+            <b-table :items="itemsGlobal" :fields="fieldsGlobal" bordered sticky-header="700px"></b-table>
           </div>
         </b-card>
       </b-card-group>
@@ -30,20 +38,60 @@ export default {
   },
   data() {
       return {
-        fields: fields,
-        items: []
+        fieldsId: fields,
+        itemsId: [],
+        itemsGlobal: [],
+        fieldsGlobal: [
+          {
+            key: 'OBJECTID',
+            label: 'ID Negara',
+            sortable: true
+          },
+          {
+            key: 'Country_Region',
+            label: 'Nama Negara',
+            sortable: true
+          },
+          {
+            key: 'Confirmed',
+            label: 'Positif',
+            sortable: true
+          },
+          {
+            key: 'Recovered',
+            label: 'Sembuh',
+            sortable: true
+          },
+          {
+            key: 'Deaths',
+            label: 'Meninggal',
+            sortable: true
+          },
+        ]
     }
   },
   mounted () {
     this.getDetailDataId();
+    this.getDetailGlobal();
   },
   methods: {
     async getDetailDataId() {
       try {
         const response = await axios.get('https://api.kawalcorona.com/indonesia/provinsi/');
         response.data.forEach(el => {
-          this.items.push(el.attributes); 
+          this.itemsId.push(el.attributes); 
         });
+      } catch (error) {
+        // 
+      }
+    },
+    async getDetailGlobal() {
+      try {
+        const response = await axios.get('https://api.kawalcorona.com/');
+        response.data.forEach(el => {
+          this.itemsGlobal.push(el.attributes); 
+        });
+        console.log(this.itemsGlobal);
       } catch (error) {
         // 
       }
@@ -54,5 +102,12 @@ export default {
 <style>
 #app {
   font-family: 'IBM Plex Sans';
+}
+th {
+  font-weight: 500;
+  font-size: 15px;
+}
+td {
+  font-size: 15px;
 }
 </style>
