90ececbd6cc153e05dd2da54c924e00881fefdd5
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-03-29 13:48:47 +0700

    Refactor

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <Navbar />
-    <router-view />
+  <b-navbar toggleable="lg" type="light" variant="faded" class="navbar-white" fixed="top">
+    <b-container>
+      <b-navbar-brand href="#" style="navbar-brand">COVID-19</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <b-navbar-nav class="ml-auto">
+
+          <b-navbar-nav>
+            <b-nav-item class="mr-4" to="/">Beranda</b-nav-item>
+            <b-nav-item class="mr-4" to="/news">Berita</b-nav-item>
+            <b-nav-item class="mr-1" to="/about">Tentang</b-nav-item>
+          </b-navbar-nav>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+  <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from '../src/components/Navbar'
 
 export default {
   name: 'App',
-  components: {
-    Navbar,
-  }
 }
 </script>
 <style>
 #app {
   font-family: 'IBM Plex Sans';
+}
+.navbar-white {
+  background-color: rgba(255, 255, 255, 0.65) !important;
+  box-shadow: 0 .125rem .25rem rgba(0,0,0,.075) !important;
+  backdrop-filter: saturate(180%) blur(20px);
+  }
+.navbar-brand {
+  font-weight: 500 !important;
+  font-size: 25px
+  }
+.navbar {
+  height: 70px;
 }
 </style>
