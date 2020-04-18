f8061a33f7a738bf612c049d2144f73f39f81592
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-04-18 20:14:34 +0700

    Fix shadow backtotop

--- src/App.vue
+++ src/App.vue
@@ -27,24 +27,24 @@
     <footer class="mt-5 footer">
       <h6 class="text-center">Copyright 2020 | Ihsan Nurul Habib</h6>
     </footer>
-    <back-to-top visibleoffset="500" bottom="30px">
-      <button class="btn-to-top" disabled>
+    <vm-back-top timing="ease">
+      <button class="btn-to-top">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon-to-top">
           <path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z">
           </path>
         </svg>
       </button>
-    </back-to-top>
+    </vm-back-top>
   </div>
 </template>
 
 <script>
-import BackToTop from 'vue-backtotop';
+import VmBackTop from 'vue-multiple-back-top'
 
 export default {
   name: 'App',
   components: {
-    BackToTop
+    VmBackTop
   }
 };
 </script>
@@ -91,7 +91,8 @@ body {
   line-height: 22px;
   background-color: #5aaa4e;
   color: white;
-  cursor: pointer;
+  outline: none !important;
+  border: none;
 }
 
 .icon-to-top {
