33e1554e314f2c13c789c48462335e75682f51a7
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-04-19 12:51:27 +0700

    remove icon

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import App from './App.vue';
+import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import App from './App.vue';
 import { router } from './router';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 
 new Vue({
   router,
